069a55641e1b29d5f469de96adcce57899055ffe
Rudnev Danil <danil.160398@yandex.ru>
2021-06-02 17:56:17 +0300

    Update PopularCategories.js

--- src/components/PopularCategories/PopularCategories.js
+++ src/components/PopularCategories/PopularCategories.js
@@ -11,17 +11,17 @@ import Category from "./Category";
 
 export default function () {
     return(
-        <div className="container pb-5 d-none d-lg-block">
+        <div className="container pb-5">
             <h3>Popular categories</h3>
             <div className="row pb-3" style={{backgroundColor:"#eee"}}>
-                <Category w="col-md-6" img={guns} label={"Guns"} r/>
-                <Category w="col-md-3" img={knifes}  label={"Knifes"} r/>
-                <Category w="col-md-3" img={pistols} label={"Pistols"} />
+                <Category w="col-6" img={guns} label={"Guns"} r/>
+                <Category w="col-3" img={knifes}  label={"Knifes"} r/>
+                <Category w="col-3" img={pistols} label={"Pistols"} />
             </div>
             <div className="row ">
-                <Category w="col-md-3" img={binoculars} label={"Binoculars"}  r/>
-                <Category w="col-md-3" img={fishing} label={"Fishing"}  r/>
-                <Category w="col-md-6" img={opticalSight} label={"Optical sight"} />
+                <Category w="col-3" img={binoculars} label={"Binoculars"}  r/>
+                <Category w="col-3" img={fishing} label={"Fishing"}  r/>
+                <Category w="col-6" img={opticalSight} label={"Optical sight"} />
             </div>
         </div>
     )
