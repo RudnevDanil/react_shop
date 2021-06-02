92a68757dff14dd4611340781392c3fe1bdbee5d
Rudnev Danil <danil.160398@yandex.ru>
2021-06-02 17:47:00 +0300

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,67 +5,90 @@ import "./Navbar.css"
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark position-sticky top-0">
-            <div className="container-fluid">
+        <div className="container h-100">
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark position-sticky top-0 h-100">
+                <div className="container-fluid px-0">
 
-                <Link className="navbar-brand mx-5" to="/">
-                    <img src={logo} alt='logo' style={{width:30, height: 30}} />
-                </Link>
-                <Link className="navbar-brand mx-5" to="/">
-                    Camp Shop
-                </Link>
+                    <Link className="navbar-brand" to="/">
+                        <img src={logo} alt='logo' style={{width:30, height: 30}} />
+                    </Link>
 
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-                    <span>
-                        <i className="fas fa-bars" style={{color:"#fff"}}></i>
-                    </span>
-                </button>
+                    <Link className="navbar-brand" to="/">
+                        Camp Shop
+                    </Link>
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav m-auto  mb-2 mb-lg-0" style={{fontSize: "0.7rem"}}>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span>
+                            <i className="fas fa-bars" style={{color:"#fff"}}/>
+                        </span>
+                    </button>
 
-                        <li className="nav-item">
-                            <Link className="nav-link active text-uppercase mx-3" to="/">
-                                Home&nbsp;<i className="fas fa-home"></i>
-                            </Link>
-                        </li>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto" style={{width: "100%", fontSize: "0.85rem"}}>
 
-                        <li className="nav-item">
-                            <Link className="nav-link text-uppercase mx-3" to="/catalog">
-                                Catalog
-                            </Link>
-                        </li>
+                            <li className="nav-item mx-lg-2">
+                                <Link className="nav-link active text-uppercase d-flex" to="/">
+                                    <div className="d-none d-lg-block">
+                                        Home&nbsp;<i className="fas fa-home"/>
+                                    </div>
+                                    <div className="d-lg-none">
+                                        Home&nbsp;
+                                    </div>
+                                </Link>
+                            </li>
 
-                        <li className="nav-item">
-                            <Link className="nav-link text-uppercase mx-3" to="/contacts">
-                                Contacts
-                            </Link>
-                        </li>
+                            <li className="nav-item mx-lg-2">
+                                <Link className="nav-link text-uppercase" to="/catalog">
+                                    Catalog
+                                </Link>
+                            </li>
 
-                        <li className="nav-item">
-                            <Link className="nav-link text-uppercase mx-3" to="/delivery">
-                                Delivery&nbsp;<i className="fas fa-truck"></i>
-                            </Link>
-                        </li>
+                            <li className="nav-item mx-lg-2">
+                                <Link className="nav-link text-uppercase" to="/contacts">
+                                    Contacts
+                                </Link>
+                            </li>
 
-                        <li className="nav-item">
-                            <Link className="nav-link text-uppercase mx-3" to="/payment">
-                                Payment&nbsp;<i className="fas fa-money-bill-alt"></i>
-                            </Link>
-                        </li>
+                            <li className="nav-item mx-lg-2">
+                                <Link className="nav-link text-uppercase" to="/delivery">
+                                    <div className="d-none d-lg-block">
+                                        Delivery&nbsp;<i className="fas fa-truck"/>
+                                    </div>
+                                    <div className="d-lg-none">
+                                        Delivery&nbsp;
+                                    </div>
+                                </Link>
+                            </li>
 
-                        <li className="nav-item">
-                            <Link className="nav-link text-uppercase mx-3" to="/cart">
-                                Cart&nbsp;<i className="fas fa-shopping-cart"></i>
-                            </Link>
-                        </li>
+                            <li className="nav-item mx-lg-2">
+                                <Link className="nav-link text-uppercase" to="/payment">
+                                    <div className="d-none d-lg-block">
+                                        Payment&nbsp;<i className="fas fa-money-bill-alt"/>
+                                    </div>
+                                    <div className="d-lg-none">
+                                        Payment&nbsp;
+                                    </div>
+                                </Link>
+                            </li>
 
-                    </ul>
+                            <li className="nav-item mx-lg-2">
+                                <Link className="nav-link text-uppercase" to="/cart">
+                                    <div className="d-none d-lg-block">
+                                        Cart&nbsp;<i className="fas fa-shopping-cart"/>
+                                    </div>
+                                    <div className="d-lg-none">
+                                        Cart&nbsp;
+                                    </div>
+                                </Link>
+                            </li>
+
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     )
 }
 
